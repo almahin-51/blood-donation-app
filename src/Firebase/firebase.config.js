d99bb05637aa7c83835ccc21fd94175c8be775ca
monibBormon <monibbormon14@gmail.com>
2022-01-03 12:30:53 +0600

    firebase and login init

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDafA-T2JzmhpNLma8xyEp7f0ayTUstSv0",
+    authDomain: "blood-home-bd.firebaseapp.com",
+    projectId: "blood-home-bd",
+    storageBucket: "blood-home-bd.appspot.com",
+    messagingSenderId: "975821716169",
+    appId: "1:975821716169:web:394ced8b897c7ef3e72b18"
+};
+
+export default firebaseConfig;
