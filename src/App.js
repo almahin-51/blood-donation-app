3db7a4d7a8c91f38be60d3889d0119e17e21d29f
monibBormon <monibbormon14@gmail.com>
2022-01-03 13:12:23 +0600

    login and register page ui done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './features/Authentication/Login/Login';
+import Registration from './features/Authentication/Registration/Registration';
 import BecomeDonar from './features/BecomeDonar/BecomeDonar';
 import FindDonar from './features/FindDonar/FindDonar';
 import Home from './features/Home/Home';
@@ -13,6 +15,8 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='find-donar' element={<FindDonar />} />
           <Route path='become-donar' element={<BecomeDonar />} />
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Registration />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Hooks/useFirebase.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import firebaseInit from '../Firebase/firebase.init';
+
+
+// firebase init 
+firebaseInit()
+const useFirebase = () => {
+
+
+};
+
+export default useFirebase;
