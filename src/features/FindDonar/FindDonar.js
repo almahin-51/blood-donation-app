ee3aa575498cab151c941ab3d5d0bf49129f3bb2
mohammad-aarif <arifbd354@gmail.com>
2022-01-03 11:19:38 +0600

    All Blood Group and All District added on Find Donar

--- src/features/FindDonar/FindDonar.js
+++ src/features/FindDonar/FindDonar.js
@@ -28,15 +28,83 @@ const FindDonar = () => {
                     <div className="col-start-2 col-span-4">
                         <div className="donar-search-box">
                             <select onChange={e => setBloodGroupText(e.target.value)}>
-                                <option value="o+">O+</option>
-                                <option value="a+">a+</option>
-                                <option value="B+">B+</option>
+                                    <option selected disabled>Select Blood Group</option>
+                                    <option value="a+">A+</option>
+                                    <option value="a-">A-</option>
+                                    <option value="b+">B+</option>
+                                    <option value="b-">B-</option>
+                                    <option value="ab+">AB+</option>
+                                    <option value="ab-">AB-</option>
+                                    <option value="o+">O+</option>
+                                    <option value="o-">O-</option>
                             </select>
                             <select onChange={e => setDistrictText(e.target.value)}>
-                                <option value="patuakhali">Patuakhali</option>
-                                <option value="thakurgaon">Thakurgaon</option>
-                                <option value="gazipur">Gazipur</option>
-                            </select>
+                            <option disabled selected>Select District</option>
+                                    <option value='Bagerhat'>Bagerhat</option>
+                                    <option value='Bandarban'>Bandarban</option>
+                                    <option value='Barguna'>Barguna</option>
+                                    <option value='Barisal'>Barisal</option>
+                                    <option value='Bhola'>Bhola</option>
+                                    <option value='Bogra'>Bogra</option>
+                                    <option value='Brahmanbaria'>Brahmanbaria</option>
+                                    <option value='Chandpur'>Chandpur</option>
+                                    <option value='Chittagong'>Chittagong</option>
+                                    <option value='Chuadanga'>Chuadanga</option>
+                                    <option value='Comilla'>Comilla</option>
+                                    <option value='CoxsBazar'>Cox'sBazar</option>
+                                    <option value='Dhaka'>Dhaka</option>
+                                    <option value='Dinajpur'>Dinajpur</option>
+                                    <option value='Faridpur'>Faridpur</option>
+                                    <option value='Feni'>Feni</option>
+                                    <option value='Gaibandha'>Gaibandha</option>
+                                    <option value='Gazipur'>Gazipur</option>
+                                    <option value='Gopalganj'>Gopalganj</option>
+                                    <option value='Habiganj'>Habiganj</option>
+                                    <option value='Jamalpur'>Jamalpur</option>
+                                    <option value='Jessore'>Jessore</option>
+                                    <option value='Jhalokati'>Jhalokati</option>
+                                    <option value='Jhenaidah'>Jhenaidah</option>
+                                    <option value='Joypurhat'>Joypurhat</option>
+                                    <option value='Khagrachari'>Khagrachari</option>
+                                    <option value='Khulna'>Khulna</option>
+                                    <option value='Kishoreganj'>Kishoreganj</option>
+                                    <option value='Kurigram'>Kurigram</option>
+                                    <option value='Kushtia'>Kushtia</option>
+                                    <option value='Lakshmipur'>Lakshmipur</option>
+                                    <option value='Lalmonirhat'>Lalmonirhat</option>
+                                    <option value='Madaripur'>Madaripur</option>
+                                    <option value='Magura'>Magura</option>
+                                    <option value='Manikganj'>Manikganj</option>
+                                    <option value='Maulvibazar'>Maulvibazar</option>
+                                    <option value='Meherpur'>Meherpur</option>
+                                    <option value='Munshiganj'>Munshiganj</option>
+                                    <option value='Mymensingh'>Mymensingh</option>
+                                    <option value='Naogaon'>Naogaon</option>
+                                    <option value='Narail'>Narail</option>
+                                    <option value='Narayanganj'>Narayanganj</option>
+                                    <option value='Narsingdi'>Narsingdi</option>
+                                    <option value='Natore'>Natore</option>
+                                    <option value='Nawabganj'>Nawabganj</option>
+                                    <option value='Netrokona'>Netrokona</option>
+                                    <option value='Nilphamari'>Nilphamari</option>
+                                    <option value='Noakhali'>Noakhali</option>
+                                    <option value='Pabna'>Pabna</option>
+                                    <option value='Panchagarh'>Panchagarh</option>
+                                    <option value='Patuakhali'>Patuakhali</option>
+                                    <option value='Pirojpur'>Pirojpur</option>
+                                    <option value='Rajbari'>Rajbari</option>
+                                    <option value='Rajshahi'>Rajshahi</option>
+                                    <option value='Rangamati'>Rangamati</option>
+                                    <option value='Rangpur'>Rangpur</option>
+                                    <option value='Satkhira'>Satkhira</option>
+                                    <option value='Shariatpur'>Shariatpur</option>
+                                    <option value='Sherpur'>Sherpur</option>
+                                    <option value='Sirajgonj'>Sirajgonj</option>
+                                    <option value='Sunamganj'>Sunamganj</option>
+                                    <option value='Sylhet'>Sylhet</option>
+                                    <option value='Tangail'>Tangail</option>
+                                    <option value='Thakurgaon'>Thakurgaon</option> 
+                             </select>
                             <input onChange={e => setAreaText(e.target.value)} className='border-2 border-red-200 rounded-full focus:outline-none w-11/12 lg:w-3/5 h-8 lg:h-10 pl-5 pr-24' type="text" placeholder='Search donar' />
                             <button  className='bg-red-400 text-white px-4 lg:px-6 py-1 lg:py-2 lg:font-semibold rounded-full -ml-20 mt-1'><i className="fas fa-search"></i> Find</button>
                         </div>
