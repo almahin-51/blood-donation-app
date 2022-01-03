e80aeba5643e0f32bd5dedb78d9dc79cb10fdce0
almahin-51 <almahin01751@gmail.com>
2022-01-03 13:17:28 +0600

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import FindDonar from './features/FindDonar/FindDonar';
-import Home from './features/Home/Home';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import FindDonar from "./features/FindDonar/FindDonar";
+import Home from "./features/Home/Home";
+import Navbar from "./features/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='find-donar' element={<FindDonar />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="find-donar" element={<FindDonar />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/features/Home/Home.js
+++ src/features/Home/Home.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import Slider from '../Slider/Slider';
+import React from "react";
+import Slider from "../Slider/Slider";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-            <h4>updated by arif
-            </h4>
-            <h2 className='text-4xl text-red-400'>Changed by Monib Bormon test branch</h2>
-            <Slider />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Home</h1>
+      <h4>updated by arif</h4>
+      <h2 className="text-4xl text-red-400">
+        Changed by Monib Bormon test branch
+      </h2>
+      <Slider />
+    </div>
+  );
 };
 
 export default Home;
