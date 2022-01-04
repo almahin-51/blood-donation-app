ff27eced806c25508cd9e2996b0d73909cc2e35a
mohammad-aarif <arifbd354@gmail.com>
2022-01-04 11:18:08 +0600

    logout button

--- src/features/Navbar/Navbar.js
+++ src/features/Navbar/Navbar.js
@@ -36,7 +36,7 @@ const Navbar = () => {
               <Link to={"/contact"}>Contact Us</Link>
             </li>
           </ul>
-          {user.email ? <img src={user.photoURL} alt="" />:<button
+          {user.displayName ? <img onClick={() => setToggle(!toggle)} className="nav-profile-img" src={user.photoURL} alt="" />:<button
             onClick={() => setToggle(!toggle)}
             className=" px-4 py-2 my-2 mr-2 rounded-md border-2 border-red-400 hover:bg-red-400 hover:text-white transition-all duration-200">
              <p><i className="far fa-user mr-2"></i>LogIn</p>
