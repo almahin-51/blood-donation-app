8f8717831a73eb2a52442c0a51d27da148ba3b52
monibBormon <monibbormon14@gmail.com>
2022-01-04 12:16:30 +0600

    hero area done

--- src/features/Home/Home.js
+++ src/features/Home/Home.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import useAuth from '../../Hooks/useAuth';
+import Hero from './Hero/Hero';
 
 const Home = () => {
-    const {user} = useAuth()
+    const { user } = useAuth()
     console.log(user);
     return (
         <div>
-            <h1>HomePage</h1>
-            <Link to={`/become-donar`}>
-                <button className='bg-red-400 px-8 py-2 mt-10 text-white font-semibold rounded mr-5'>Want to be a donar?</button>
-            </Link>
-            <Link to={`/find-donar`}><button className='bg-red-400 px-8 py-2 mt-10 text-white font-semibold rounded'>Find Donar</button></Link>
+            <Hero />
         </div>
     );
 };
