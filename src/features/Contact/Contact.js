721709ea7a8734a17c118aaefbfe63329e19216e
monibBormon <monibbormon14@gmail.com>
2022-01-04 18:23:45 +0600

    title and favicon set

--- src/features/Contact/Contact.js
+++ src/features/Contact/Contact.js
@@ -9,15 +9,15 @@ const Contact = () => {
             </div >
             <div className="container mx-auto py-10 px-5 lg:px-0">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    <div className="contact-box rounded shadow-md text-center p-5">
+                    <div className="contact-box hover:shadow-lg transition-all duration-300 rounded shadow-md text-center p-5">
                         <i className="fas fa-envelope text-4xl text-red-500 mb-3"></i>
                         <p className='font-semibold'>blood@bd.com</p>
                     </div>
-                    <div className="contact-box rounded shadow-md text-center p-5">
+                    <div className="contact-box hover:shadow-lg transition-all duration-300 rounded shadow-md text-center p-5">
                         <i className="fas fa-location-arrow text-4xl text-red-500 mb-3"></i>
                         <p className='font-semibold'> Road-1/2,Mirpur,Dhaka</p>
                     </div>
-                    <div className="contact-box rounded shadow-md text-center p-5">
+                    <div className="contact-box hover:shadow-lg transition-all duration-300 rounded shadow-md text-center p-5">
                         <i className="fas fa-phone-alt text-4xl text-red-500 mb-3"></i>
                         <p className='font-semibold'>+880199999922</p>
                     </div>
